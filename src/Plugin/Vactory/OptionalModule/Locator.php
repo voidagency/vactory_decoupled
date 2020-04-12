40c4fd5ec114a081ef96b28b2b4529132133a32b
Hamza Bahlaouane <h.bahlaouane@void.fr>
2020-04-12 22:30:29 +0100

    Locator Optional module.

---
+++ src/Plugin/Vactory/OptionalModule/Locator.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Drupal\vactory_decoupled\Plugin\Vactory\OptionalModule;
+
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * News.
+ *
+ * @VactoryOptionalModule(
+ *   id = "vactory_locator",
+ *   label = @Translation("Locator"),
+ *   description = @Translation("The locator module adds an entity model and default content for maps"),
+ *   type = "module",
+ *   weight = 10,
+ *   standardlyEnabled = true,
+ * )
+ */
+class Locator extends AbstractOptionalModule {
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(array $form, FormStateInterface $form_state) {
+
+        $form = parent::buildForm($form, $form_state);
+
+        $form['vactory_locator']['project_info'] = [
+            '#type' => 'item',
+            '#description' => $this->t("The locator module adds an entity model and default content for maps"),
+        ];
+
+        return $form;
+    }
+
+}
