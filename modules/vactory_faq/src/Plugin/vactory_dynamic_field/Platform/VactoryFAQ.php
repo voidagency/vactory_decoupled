2b4248b4d92ed47f7daffee2a0c0596e6839449c
Hamza Bahlaouane <h.bahlaouane@void.fr>
2021-02-23 18:15:37 +0100

    FAQ

---
+++ modules/vactory_faq/src/Plugin/vactory_dynamic_field/Platform/VactoryFAQ.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Drupal\vactory_faq\Plugin\vactory_dynamic_field\Platform;
+
+use Drupal\vactory_dynamic_field\VactoryDynamicFieldPluginBase;
+
+/**
+ * A DF provider plugin.
+ *
+ * @PlatformProvider(
+ *   id = "vactory_faq",
+ *   title = @Translation("FAQ")
+ * )
+ */
+class VactoryFAQ extends VactoryDynamicFieldPluginBase {
+
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, $widgetsPath) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition, drupal_get_path('module', 'vactory_faq') . '/widgets');
+  }
+
+}
