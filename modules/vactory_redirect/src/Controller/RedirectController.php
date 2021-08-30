bae9d5d6d23d89c55b29beb6e8ce67c6a4f541fe
Hamza Bahlaouane <h.bahlaouane@void.fr>
2021-08-30 18:50:30 +0100

    Update redirect

--- modules/vactory_redirect/src/Controller/RedirectController.php
+++ modules/vactory_redirect/src/Controller/RedirectController.php
@@ -1,39 +1,37 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: ismail
- * Date: 30/03/2021
- * Time: 13:18
- */
 
 namespace Drupal\vactory_redirect\Controller;
 
 use \Drupal\Core\Controller\ControllerBase;
+use Drupal\file\Entity\File;
 use \Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Request;
+use League\Csv\Reader;
 
 class RedirectController extends ControllerBase
 {
 
-    public function index()
-    {
-        $dir = \Drupal::service('file_system')->realpath("public://vactory_redirect");
-        $scanned_directory = array_diff(scandir($dir,1), array('..', '.'));
-        $csv_file = \Drupal::service('file_system')->realpath("public://vactory_redirect/".$scanned_directory[0]);
-        $file_content = file_get_contents($csv_file);
-        $array = array_map("str_getcsv", explode("\n", $file_content));
-        $redirections = array();
-        for ($i = 1; $i < count($array); ++$i) {
-            $item = array(
-                $array[0][0] => $array[$i][0],
-                $array[0][1] => $array[$i][1],
-                $array[0][2] => $array[$i][2],
-            );
-            array_push($redirections, $item);
-        }
-
-        return new JsonResponse([
-            "redirections" => $redirections]
-        );
+  public function index()
+  {
+    $fid = (int) \Drupal::config('vactory_redirect.settings')->get('fid');
+    $file = $file = File::load($fid);
+    if (!$file) {
+      return new JsonResponse([
+          "redirections" => []
+      ]);
     }
+
+    $csv_file = \Drupal::service('file_system')->realpath($file->getFileUri());
+    $csv = Reader::createFromPath($csv_file, 'r');
+    $csv->setHeaderOffset(0);
+    $records = $csv->getRecords();
+    $redirections = array();
+
+    foreach ($records as $record) {
+      array_push($redirections, $record);
+    }
+
+    return new JsonResponse([
+        "redirections" => $redirections]
+    );
+  }
 }
--- modules/vactory_redirect/src/Form/VactoryRedirectSettingsForm.php
+++ modules/vactory_redirect/src/Form/VactoryRedirectSettingsForm.php
@@ -7,74 +7,90 @@
  */
 
 namespace Drupal\vactory_redirect\Form;
+
 use \Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Url;
+use Drupal\file\Entity\File;
 
 class VactoryRedirectSettingsForm extends ConfigFormBase
 {
 
-    /**
-     * Gets the configuration names that will be editable.
-     *
-     * @return array
-     *   An array of configuration object names that are editable if called in
-     *   conjunction with the trait's config() method.
-     */
-    protected function getEditableConfigNames()
-    {
-        return [
-            'vactory_redirect.settings'
-        ];
+  /**
+   * Gets the configuration names that will be editable.
+   *
+   * @return array
+   *   An array of configuration object names that are editable if called in
+   *   conjunction with the trait's config() method.
+   */
+  protected function getEditableConfigNames()
+  {
+    return [
+      'vactory_redirect.settings'
+    ];
+  }
+
+  /**
+   * Returns a unique string identifying the form.
+   *
+   * The returned ID should be a unique string that can be a valid PHP function
+   * name, since it's used in hook implementation names such as
+   * hook_form_FORM_ID_alter().
+   *
+   * @return string
+   *   The unique string identifying the form.
+   */
+  public function getFormId()
+  {
+    return 'vactory_redirect_admin_settings';
+  }
+
+  public function buildForm(array $form, FormStateInterface $form_state)
+  {
+    $url_options = array('absolute' => TRUE);
+    $t_args = [
+      ':settings_url' => Url::fromUri('base:/admin/structure/file-types/manage/document/edit', $url_options)->toString(),
+    ];
+    $message = t('If your having trouble uploading the csv file. Add <strong><em>text/csv</em></strong> <a href=":settings_url"> to the allowed <em>MIME types</em></a>.', $t_args);
+    \Drupal::messenger()->addWarning($message);
+
+    // Check file.
+    $fid = $this->config('vactory_redirect.settings')->get('fid');
+    $fids = [];
+    if ($fid && $fid > 0 && ($file = File::load($fid))) {
+      $fids[0] = $file->id();
     }
 
-    /**
-     * Returns a unique string identifying the form.
-     *
-     * The returned ID should be a unique string that can be a valid PHP function
-     * name, since it's used in hook implementation names such as
-     * hook_form_FORM_ID_alter().
-     *
-     * @return string
-     *   The unique string identifying the form.
-     */
-    public function getFormId()
-    {
-        return 'vactory_redirect_admin_settings';
-    }
-
-    public function buildForm(array $form, FormStateInterface $form_state)
-    {
-
-        $validators = array(
-            'file_validate_extensions' => array('csv'),
-        );
-
-        $form['redirect'] = array(
-        '#type' => 'managed_file',
-        '#name' => 'redirect',
-        '#title' => t('redirection file'),
-        '#description' => t('Upload redirect file (.csv).'),
-        '#upload_validators' => $validators,
-        '#upload_location' => 'public://vactory_redirect',
+    $validators = array(
+      'file_validate_extensions' => array('csv'),
     );
 
-        $form['array_filter'] = array('#type' => 'value', '#value' => TRUE);
-        return parent::buildForm($form, $form_state);
-    }
+    $form['redirect'] = array(
+      '#type' => 'managed_file',
+      '#name' => 'redirect',
+      '#title' => t('CSV file'),
+      '#description' => t('Upload redirect file (.csv).'),
+      '#upload_validators' => $validators,
+      '#upload_location' => 'public://vactory_redirect',
+      '#default_value' => $fids,
+      '#required' => TRUE,
+    );
 
-    public function submitForm(array &$form, FormStateInterface $form_state)
-    {
-        $fid = reset($form_state->getValue('redirect'));
-        $database = \Drupal::database();
-        $query = $database->query("SELECT fid FROM {file_managed}");
-        $result = $query->fetchAll();
-        $array = json_decode(json_encode($result), true);
-        foreach ($array as $item){
-            if ($item['fid'] != $fid){
-                file_delete($item['fid']);
-            }
-        }
-        drupal_set_message('file has been uploaded.');
+    return parent::buildForm($form, $form_state);
+  }
 
+  public function submitForm(array &$form, FormStateInterface $form_state)
+  {
+    $fid = reset($form_state->getValue('redirect'));
+    $this->config('vactory_redirect.settings')
+      ->set('fid', $fid)
+      ->save();
+    $file = File::load($fid);
+    // Make sure that a used file is permanent.
+    if (!$file->isPermanent()) {
+      $file->setPermanent();
+      $file->save();
     }
+    \Drupal::messenger()->addMessage('file has been uploaded.');
+  }
 }
