2ad1bccb2fb9344f8baf3a6e08cce4b9ede480c3
Brahim KHOUY <b.khouy@void.fr>
2022-02-16 10:49:42 +0100

    Fix: #32498 - Update patches & replace getUsername() deprecated method.

--- modules/vactory_jsonapi/src/Plugin/Field/InternalUserEntityFieldItemList.php
+++ modules/vactory_jsonapi/src/Plugin/Field/InternalUserEntityFieldItemList.php
@@ -74,7 +74,7 @@ class InternalUserEntityFieldItemList extends FieldItemList
         ];
       }
 
-      $user_fullname = $user->getUserName();
+      $user_fullname = $user->getDisplayName();
       $author_first_name = $user->get('field_first_name')->getString();
       $author_last_name = $user->get('field_last_name')->getString();
 
@@ -87,7 +87,7 @@ class InternalUserEntityFieldItemList extends FieldItemList
 
       $value[$name] = [
         'id' => $user->id(),
-        'name' => $user->getUsername(),
+        'name' => $user->getDisplayName(),
         'first_name' => $user->get('field_first_name')->getString(),
         'last_name' => $user->get('field_last_name')->getString(),
         'full_name' => $user_fullname,
